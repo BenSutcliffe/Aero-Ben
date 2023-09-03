3d9bc3e1b783daf524eaf23aaba50cd3a530df07
BenSutcliffe <bps29@cam.ac.uk>
2023-09-03 16:56:01 +0100

    data read in

---
+++ FlightProfileData.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_excel("FlightProfileDataClone.xlsx")
+
+time_array = np.array(df.iloc[:, 0])
+x_array = np.array(df.iloc[:, 1])
+y_array = np.array(df.iloc[:, 2])
+z_array = np.array(df.iloc[:, 3])
+vx_array = np.array(df.iloc[:, 4])
+vy_array = np.array(df.iloc[:, 5])
+vz_array = np.array(df.iloc[:, 6])
+ax_array = np.array(df.iloc[:, 7])
+ay_array = np.array(df.iloc[:, 8])
+az_array = np.array(df.iloc[:, 9])
+e0_array = np.array(df.iloc[:, 10])
+e1_array = np.array(df.iloc[:, 11])
+e2_array = np.array(df.iloc[:, 12])
+e3_array = np.array(df.iloc[:, 13])
+mach_array = np.array(df.iloc[:, 14])
+q_array = np.array(df.iloc[:, 15])
+pressure_array = np.array(df.iloc[:, 16])
+density_array = np.array(df.iloc[:, 17])
+
